045511442995c4efbcd63a6a8d2afa860c4a1e59
Camille Baldock <camille@camillebaldock.co.uk>
2015-01-16 15:06:15 -0800

    Add dock definition script

---
+++ dock-script.bash
@@ -0,0 +1,7 @@
+defaults delete com.apple.dock persistent-apps
+defaults delete com.apple.dock persistent-others
+
+# Add Safari
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+killall -HUP Dock
