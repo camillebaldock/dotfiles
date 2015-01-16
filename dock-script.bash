f34504feea216c5153c71573292e75b8c1424098
Camille Baldock <camille@camillebaldock.co.uk>
2015-01-16 15:17:39 -0800

    Correct entry for Messages

--- dock-script.bash
+++ dock-script.bash
@@ -6,7 +6,7 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 # Add Airmail 2
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Airmail 2.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 # Add iMessage
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Keynote.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Messages.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 # Add Skype
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Skype.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 # Add TweetDeck
