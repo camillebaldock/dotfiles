d45d9a281d3b6e55a29119fd9124da4b2f7ebde3
camilleldn <camille@camillebaldock.co.uk>
2015-09-20 18:55:56 +0100

    remove linkapps

--- brew/linkapps.sh
+++
@@ -1,1 +0,0 @@
-brew linkapps; find ~/Applications -type l | while read f; do osascript -e "tell app \"Finder\" to make new alias file at POSIX file \"/Applications\" to POSIX file \"$(stat -f%Y "$f")\""; rm "$f";
