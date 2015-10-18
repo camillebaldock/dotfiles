e260cb10554b8764d7b735eb5d6cda35af105c87
camilleldn <camille@camillebaldock.co.uk>
2015-10-18 13:40:55 +0100

    Fitcommit

--- git/install-commit-hook.sh
+++ git/install-commit-hook.sh
@@ -1,1 +1,7 @@
 sudo gem install fit-commit
+echo "Installing fitcommit for all git repos"
+for repo in $(find ~/Code -name .git -type d -prune | rev | cut -c5- | rev); do
+  cd $repo
+  echo $repo
+  fit-commit install
+done
